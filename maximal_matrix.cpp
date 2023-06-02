c1662f9853ff730aa055b1f2a88f0bc1d7a2763a
farikunAziz <farikuna@gmail.com>
2023-06-02 09:59:35 +0700

    It's Friday

---
+++ maximal_matrix.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+constexpr int limits = 5;
+
+int maximalOne(int data[limits], int temp, int length){
+  if(length == -1)
+    return temp;
+  return maximalOne(data, (data[length] > temp ? temp = data[length] : temp), length - 1);
+}
+
+int maximalTwo(std::vector<int> &data, int temp, int length){
+  if(length == -1)
+    return temp;
+  return maximalTwo(data, (data[length] > temp ? temp = data[length] : temp), length - 1);
+}
+
+int main (int argc, char *argv[])
+{
+  
+  return 0;
+}
